70dcb7e66a34064a5588dcc14f8a5c9fa34bec44
madushanka2016 <madushankakahawa@gmail.com>
2019-09-17 02:05:07 +0530

    init

---
+++ script/home.js
@@ -0,0 +1,58 @@
+function loadHome(){
+    var mainGril = document.getElementById("mainGril");
+    mainGril.innerHTML = `
+    <div class="row justify-content-center">
+        <div class="animated slideInRight  ">
+            <div class="col">
+                <h5 style="font-size: 30px; margin-top: 50px;">Hello,I'am</h5>
+                <h1 style="font-size: 70px; ">Madushanka Kahawa</h1>						
+            </div> 	
+        </div>	
+    </div>
+    <div class="row">
+        <div class="col">
+            <p style="margin-left: 50px;">I am a software engineering undergraduate of University of Kelaniya and I am willing to learn new technology always.hands on with java, c , php and also sql html css.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <table style="margin-left: 50px;">
+                <tbody>
+                    <tr>
+                        <td width="100px" height="50px">Email:</td>
+                        <td><a href="mailto:madushankakahawa@gmail.com? subject=messageSubject" >madushankakahawa@gmail.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>Mobie:</td>
+                        <td>+94 76 694 0764</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col" style="text-align: center;">
+            <a href="https://m.facebook.com/madushanka.kahawa" target="" title="">
+                <i class="fa fa-facebook animated bounceIn" style="background-color: #3b5998;color: white;"></i>
+            </a>
+            <a href="https://github.com/madushanka2016" target="" title="">
+                <i class="fa fa-github animated bounceIn" style="color: white; background-color: #333;"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/madushanka-kahawa-521b3115a/" target="" title="">
+                <i class="fa fa-linkedin animated bounceIn" style="background-color: #0077b5;color: white;"></i>
+            </a>
+            <a href="https://stackoverflow.com/users/9475949/madushanka-kahawa" target="" title="">
+                <i class="fa fa-stack-overflow animated bounceIn" style="background-color: #f48024;color: white;"></i>
+            </a>
+            <a href="https://medium.com/@madushankakahawa" target="" title="">
+                <i class="fa fa-medium animated bounceIn" style="background-color: black;color: white;"></i>
+            </a>
+            <!-- <a href="" target="" title="">
+                <i class="fa fa-hackerrank animated bounceIn" style="background-color: red;color: black;"></i>
+            </a> -->
+        </div>
+    </div>
+
+
+    `
+}
