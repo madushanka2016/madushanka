c32e2a2f4736802f3bdb4603af28cb2fdaeddb63
madushanka2016 <madushankakahawa@gmail.com>
2019-09-17 02:05:21 +0530

    init

---
+++ script/education.js
@@ -0,0 +1,34 @@
+function loadEduction() {
+    var mainGril = document.getElementById("mainGril");
+    mainGril.innerHTML = `
+	<div>
+		<div class="row">
+			<h1 class="animated tada" 
+			style="text-align: center;width: 100%;margin-bottom: 50px;margin-top: 50px; font-size: 55px;">Education</h1>
+			<div class="col-4">
+				<img src="picture/uok.png" alt="University od kelaniya" style="display: block; margin-left: auto;margin-right: auto;padding-top: 20px;padding-bottom: 20px" width="100%">
+			</div>
+			<div class="col-8" style="display: block;margin-bottom: auto; margin-top: auto;">
+				<p class="mb" style="font-size: 18px"><b>B.SC. (Hons.) in Software Engineering - University of Kelaniya </b></p><p> ( 2018 - present )</p>
+				<ul style="font-size: 18px">Currently in Level 2 mainating the <b>GPA</b> at <b>3.12</b> out of 4.0</ul>
+			</div>
+		</div>
+		<hr>
+		<div class="row" style="margin-top: 20px;margin-bottom: 50px;">
+			<div class="col-4">
+				<img src="picture/vcg.jpg" alt="" style="display: block;margin: auto;margin-top: 10px;margin-bottom: 10px" width="100%" >
+			</div>
+			<div class="col" >
+				<div style="margin: 20px 0px 40px 0px">
+					<p class="mb" style="font-size: 18px"><b>Advance level - Vidyaloka College , Galle </b> ( 2016 )</p>
+					<ul style="font-size: 18px"><pre>Combind Maths: A , Physics: B , Chemistry: C  |  Z-1.5764</pre></ul>
+				</div>
+				<hr>
+				<div style="margin: 20px 0px 20px 0px;">
+					<p class="mb" style="font-size: 18px"><b>Ordinary Level - Vidyaloka College , Galle </b> ( 2013 )</p>
+				</div>
+			</div>
+		</div>
+	</div>
+    `
+}
