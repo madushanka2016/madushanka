4ba07df3cf9d8131540d3fa0cdd7079e867e200e
madushanka2016 <madushankakahawa@gmail.com>
2019-09-17 02:05:43 +0530

    init

---
+++ script/skil.js
@@ -0,0 +1,65 @@
+function loadSkill() {
+    var mainGril = document.getElementById("mainGril");
+    mainGril.innerHTML = `
+	<div>
+		<div>
+			<h1 class="animated tada"
+			style="text-align: center;margin-bottom: 40px;margin-top: 20px; font-size: 55px;">Skill</h1>
+		</div>
+		<div class="row">
+			<!-- <div class="col-2"></div> -->
+			<div class="col">
+				<h4 style="text-align: center;margin-top: 20px">Tecnical skill</h4>
+				<div class="row" style="height: 175px;">
+					<div class="col-3">
+						<p style="text-align: center">
+							<img  src="picture/java.png" alt="Java" height="100%">
+						</p>
+					</div>
+					<div class="col-3">
+						<p style="text-align: center">
+							<img src="picture/php.png" alt="Java" width="100%"
+							style="margin-top: 40px">
+						</p>
+					</div>
+					<div class="col-3">
+						<p style="text-align: center">
+							<img src="picture/python.jpg" alt="Java" height="100%">
+						</p>
+					</div>
+					<div class="col-3">
+						<p style="text-align: center">
+							<img src="picture/js.png" alt="Java" width="100%"
+							style="margin-top: 40px">
+						</p>
+					</div>	
+				</div>
+				<hr>
+				<div class="row" style="height: 200px">
+					<div class="col-3">
+						<p style="text-align: center">
+							
+						</p>
+					</div>
+					<div class="col-3">
+						<p style="text-align: center">
+							
+						</p>
+					</div>
+					<div class="col-3">
+						<p style="text-align: center">
+							
+						</p>
+					</div>
+					<div class="col-3">
+						<p style="text-align: center">
+							
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>	
+	</div>
+        
+    `;
+}
